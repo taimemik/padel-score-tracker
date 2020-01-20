07797f8f38ef447e8c670c33c17f92406cca9894
Mika Taimen <mika.taimen@arado.fi>
2020-01-20 17:00:11 +0200

    Updates

---
+++ precache-manifest.57960b700920e8c4cbb7e7bcee54ca8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802f5cb9191f49d039962cd5ac794121",
+    "url": "/padel-score-tracker/index.html"
+  },
+  {
+    "revision": "7e08f2627345a0a3e5e6",
+    "url": "/padel-score-tracker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8d4a15044cd2aea171c2",
+    "url": "/padel-score-tracker/static/js/2.148ba23b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/padel-score-tracker/static/js/2.148ba23b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e08f2627345a0a3e5e6",
+    "url": "/padel-score-tracker/static/js/main.5e3d7628.chunk.js"
+  },
+  {
+    "revision": "94933d2df46c07e2121d",
+    "url": "/padel-score-tracker/static/js/runtime-main.8d7e12c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/padel-score-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
