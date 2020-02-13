7a89e4ca4e1ecc6046093aae9566a2f5df620354
Mika Taimen <mika.taimen@arado.fi>
2020-02-13 21:22:49 +0200

    set selection

--- src/components/Home/results.js
+++ src/components/Home/results.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
-import { withAuthorization } from "../Session";
-import Button from "@material-ui/core/Button";
-import Container from "@material-ui/core/Container";
 import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
-import { compose } from "recompose";
-import { withFirebase } from "../Firebase";
-import { AuthUserContext } from "../Session";
-import Firebase from "firebase";
 import IconButton from "@material-ui/core/IconButton";
 import Avatar from "@material-ui/core/Avatar";
 import Tooltip from "@material-ui/core/Tooltip";
+import SetResult from "./setResult";
 
 const INITIAL_STATE = {
-  games: ["0", "1", "2", "3", "4", "5", "6", "7"],
+  result1: 0,
+  result2: 0,
 };
 
 class Results extends Component {
@@ -33,57 +27,83 @@ class Results extends Component {
     </div>
   );
 
+  setTeamResult1 = newValue => {
+    console.log("result1" + newValue);
+    this.setState({ result1: newValue });
+  };
+
+  setTeamResult2 = value => {
+    console.log("result2" + value);
+    this.setState({ result2: value });
+  };
+
   render() {
     return (
       <div>
         {this.props.team2 &&
           this.props.team2.player2 &&
           this.props.team2.player2.uid && (
-            <div className="userSelection">
+            <div>
               <div className="userSelection">
-                <Tooltip title={this.props.team1.player1.username} disabled>
-                  <Avatar
-                    className="userAvatar"
-                    style={{
-                      backgroundColor: this.props.team1.player1.color,
-                    }}
-                  >
-                    {this.props.team1.player1.initials}
-                  </Avatar>
-                </Tooltip>
-                <Tooltip title={this.props.team1.player2.username} disabled>
-                  <Avatar
-                    className="userAvatar"
-                    style={{
-                      backgroundColor: this.props.team1.player2.color,
-                    }}
-                  >
-                    {this.props.team1.player2.initials}
-                  </Avatar>
-                </Tooltip>
-                VS
-                <Tooltip title={this.props.team2.player1.username} disabled>
-                  <Avatar
-                    className="userAvatar"
-                    style={{
-                      backgroundColor: this.props.team2.player1.color,
-                    }}
-                  >
-                    {this.props.team2.player1.initials}
-                  </Avatar>
-                </Tooltip>
-                <Tooltip title={this.props.team2.player2.username} disabled>
-                  <Avatar
-                    className="userAvatar"
-                    style={{
-                      backgroundColor: this.props.team2.player2.color,
-                    }}
-                  >
-                    {this.props.team2.player2.initials}
-                  </Avatar>
-                </Tooltip>
+                <div className="userSelection">
+                  <Tooltip title={this.props.team1.player1.username} disabled>
+                    <Avatar
+                      className="userAvatar"
+                      style={{
+                        backgroundColor: this.props.team1.player1.color,
+                      }}
+                    >
+                      {this.props.team1.player1.initials}
+                    </Avatar>
+                  </Tooltip>
+                  <Tooltip title={this.props.team1.player2.username} disabled>
+                    <Avatar
+                      className="userAvatar"
+                      style={{
+                        backgroundColor: this.props.team1.player2.color,
+                      }}
+                    >
+                      {this.props.team1.player2.initials}
+                    </Avatar>
+                  </Tooltip>
+                  VS
+                  <Tooltip title={this.props.team2.player1.username} disabled>
+                    <Avatar
+                      className="userAvatar"
+                      style={{
+                        backgroundColor: this.props.team2.player1.color,
+                      }}
+                    >
+                      {this.props.team2.player1.initials}
+                    </Avatar>
+                  </Tooltip>
+                  <Tooltip title={this.props.team2.player2.username} disabled>
+                    <Avatar
+                      className="userAvatar"
+                      style={{
+                        backgroundColor: this.props.team2.player2.color,
+                      }}
+                    >
+                      {this.props.team2.player2.initials}
+                    </Avatar>
+                  </Tooltip>
+                </div>
               </div>
-              {this.resultSelection()}
+              <div className="userSelection">
+                <SetResult
+                  setTeamResult={this.setTeamResult1}
+                  result={this.state.result1}
+                  name={"result1"}
+                  isInverted={false}
+                />
+                <span> -</span>
+                <SetResult
+                  setTeamResult={this.setTeamResult2}
+                  result={this.state.result2}
+                  name={"result2"}
+                  isInverted={true}
+                />
+              </div>
             </div>
           )}
       </div>
---
+++ src/components/Home/setResult.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+
+import PropTypes from "prop-types";
+
+import Rating from "@material-ui/lab/Rating";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import Box from "@material-ui/core/Box";
+
+const INITIAL_STATE = {
+  games: ["0", "1", "2", "3", "4", "5", "6", "7"],
+  hover: -1,
+};
+
+const StyledRating = withStyles({
+  iconFilled: {
+    color: "#ff6d75",
+  },
+  iconHover: {
+    color: "#ff3d47",
+  },
+})(Rating);
+
+class SetResult extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
+
+  setHover = newValue => {
+    this.setState({ hover: newValue });
+  };
+
+  render() {
+    return (
+      <div
+        className={
+          this.props.isInverted ? "invertedScoreSelection" : "scoreSelection"
+        }
+      >
+        <StyledRating
+          name={this.props.name}
+          max={7}
+          defaultValue={0}
+          value={this.props.result}
+          onChange={(event, newValue) => {
+            this.props.setTeamResult(newValue);
+          }}
+          onChangeActive={(event, newHover) => {
+            this.setHover(newHover);
+          }}
+          precision={1}
+          icon={<FiberManualRecordIcon fontSize="inherit" />}
+        />
+        {(this.state.hover !== -1 || this.props.result !== null) && (
+          <Box ml={2} className="ratingBox">
+            {this.state.hover !== -1 ? this.state.hover : this.props.result}
+          </Box>
+        )}
+      </div>
+    );
+  }
+}
+
+SetResult.propTypes = {
+  result: PropTypes.number,
+  setTeamResult: PropTypes.func,
+  name: PropTypes.string,
+  isInverted: PropTypes.bool,
+};
+
+export default SetResult;
